fc555859467b09bb6faa7c2e5611ad199410e102
Nontawat Wuttifaey <mistidea9@gmail.com>
2021-02-27 20:09:51 +0700

    Initial commit

---
+++ src/test/java/cmru/ac/th/adminjavap1/Adminjavap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cmru.ac.th.adminjavap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adminjavap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
